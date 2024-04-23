c7f845ea9a3cdc6c13dd3c2abdea976906485ba8
hichoudhary22 <hichoudhary22@gmail.com>
2024-04-23 17:03:12 +0530

    sync

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,6 @@ const Contact = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div ref={ref} className={styles.contacts}>
       <h1>Contacts</h1>
-
       <div>
         <a href="https://www.linkedin.com/in/hichoudhary22">linkedIn</a>
         <a href="https://github.com/hichoudhary22">GitHub</a>
